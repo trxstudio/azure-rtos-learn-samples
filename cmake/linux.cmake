dd33fd55794dd49bf4ffbd1d2fbdb39914f7d2b0
Liya Du <liydu@microsoft.com>
2022-03-23 14:03:15 -0700

    Use 32bit compiling the project

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -19,10 +19,10 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(LINUX_FLAGS "-g -pthread")
 
-set(CMAKE_C_FLAGS   "${LINUX_FLAGS} " CACHE INTERNAL "c compiler flags")
-set(CMAKE_CXX_FLAGS "${LINUX_FLAGS} -fno-rtti -fno-exceptions" CACHE INTERNAL "cxx compiler flags")
+set(CMAKE_C_FLAGS   "${LINUX_FLAGS} -m32" CACHE INTERNAL "c compiler flags")
+set(CMAKE_CXX_FLAGS "${LINUX_FLAGS} -m32 -fno-rtti -fno-exceptions" CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_ASM_FLAGS "${LINUX_FLAGS} -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
-set(CMAKE_EXE_LINKER_FLAGS "${LINUX_FLAGS} ${LD_FLAGS} -Wl,--gc-sections" CACHE INTERNAL "exe link flags")
+set(CMAKE_EXE_LINKER_FLAGS "${LINUX_FLAGS} ${LD_FLAGS} -Wl,--gc-sections -melf_i386" CACHE INTERNAL "exe link flags")
 
 SET(CMAKE_C_FLAGS_DEBUG "-Og -g -ggdb3" CACHE INTERNAL "c debug compiler flags")
 SET(CMAKE_CXX_FLAGS_DEBUG "-Og -g -ggdb3" CACHE INTERNAL "cxx debug compiler flags")
---
+++ courses/threadx/ProjectHelloWorld/build/CMakeFiles/ProjectHelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectHelloWorld.dir/main.c.o"
+  "CMakeFiles/ProjectHelloWorld.dir/main.c.o.d"
+  "ProjectHelloWorld"
+  "ProjectHelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjectHelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
